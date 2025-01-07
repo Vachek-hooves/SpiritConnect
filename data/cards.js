21ff81256f4330d1b37648982dd89da70d9668b3
Vachek-hooves <vachek0602@gmail.com>
2025-01-07 11:36:09 +0200

    update data set

--- data/cards.js
+++ data/cards.js
@@ -4,21 +4,24 @@ export const meditation = [
         name: "Forest Meditation",
         duration: 25,
         image: require("../assets/images/cards/meditation1.png"),
-        text:'This practice involves meditating in nature, sitting in a forest and focusing on the sounds, smells, and sights around you. Deep connection with nature helps calm the mind, reduce stress, and enhance mindfulness. Choose a quiet spot, sit comfortably, and begin to concentrate on natural sounds like birdsong or rustling leaves.'
+        text:'This practice involves meditating in nature, sitting in a forest and focusing on the sounds, smells, and sights around you. Deep connection with nature helps calm the mind, reduce stress, and enhance mindfulness. Choose a quiet spot, sit comfortably, and begin to concentrate on natural sounds like birdsong or rustling leaves.',
+        isCompleted: false
     },
     {
         id: 2,
         name: "Candle Gazing Meditation",
         duration: 15,
         image: require("../assets/images/cards/meditation2.png"),
-        text:'Candle gazing, or Trataka, is an ancient technique that improves focus and calms the mind. Sit in front of a lit candle about a meter away. Focus your gaze on the flame without blinking for as long as you can. This practice strengthens vision, develops patience, and fosters inner harmony.'
+        text:'Candle gazing, or Trataka, is an ancient technique that improves focus and calms the mind. Sit in front of a lit candle about a meter away. Focus your gaze on the flame without blinking for as long as you can. This practice strengthens vision, develops patience, and fosters inner harmony.',
+        isCompleted: false
     },
     {
         id: 3,
         name: "Loving-Kindness Meditation",
         duration: 20,
         image: require("../assets/images/cards/meditation3.png"),
-        text:"This practice is designed to cultivate kindness and compassion. Sit in a comfortable position, close your eyes, and imagine sending kind wishes to yourself, your loved ones, and the world. Repeat phrases like 'May I be happy. May all beings be happy.' It helps reduce tension, improve emotional wellbeing, and strengthen your connection to the world."
+        text:"This practice is designed to cultivate kindness and compassion. Sit in a comfortable position, close your eyes, and imagine sending kind wishes to yourself, your loved ones, and the world. Repeat phrases like 'May I be happy. May all beings be happy.' It helps reduce tension, improve emotional wellbeing, and strengthen your connection to the world.",
+        isCompleted: false
     }
 ]
 
@@ -29,21 +32,24 @@ export const yoga = [
         name: " Sun Salutation (Surya Namaskar)",
         duration: 20,
         image: require("../assets/images/cards/yoga1.png"),
-        text:'This practice involves standing up, stretching your arms and legs, and bowing to the sun. It helps wake up your body, energize your mind, and align your body with the natural world.'
+        text:'This practice involves standing up, stretching your arms and legs, and bowing to the sun. It helps wake up your body, energize your mind, and align your body with the natural world.',
+        isCompleted: false
     },
     {
         id: 2,
         name: "Tree Pose",
         duration: 10,
         image: require("../assets/images/cards/yoga2.png"),
-        text:'Vrikshasana enhances balance and concentration. Stand straight, shift your weight onto one leg, and lift the other, placing the foot against the inner thigh of the standing leg. Join your palms together at your chest or raise them overhead. This pose strengthens leg muscles and improves posture.'
+        text:'Vrikshasana enhances balance and concentration. Stand straight, shift your weight onto one leg, and lift the other, placing the foot against the inner thigh of the standing leg. Join your palms together at your chest or raise them overhead. This pose strengthens leg muscles and improves posture.',
+        isCompleted: false
     },
     {
         id: 3,
         name: "Child’s Pose (Balasana)",
         duration: 15,
         image: require("../assets/images/cards/yoga3.png"),
-        text:'Balasana is a restorative pose that helps relieve tension in the lower back, stretch the spine, and calm the mind. Kneel on the floor, lower your hips onto your heels, and stretch your arms forward as you rest your forehead on the ground. Stay in this pose for a few minutes, focusing on your breath.'
+        text:'Balasana is a restorative pose that helps relieve tension in the lower back, stretch the spine, and calm the mind. Kneel on the floor, lower your hips onto your heels, and stretch your arms forward as you rest your forehead on the ground. Stay in this pose for a few minutes, focusing on your breath.',
+        isCompleted: false
     }
 ]
 
@@ -54,20 +60,23 @@ export const breathing = [
         name: "Diaphragmatic Breathing",
         duration: 10,
         image: require("../assets/images/cards/breath1.png"),
-        text:"Diaphragmatic breathing, or belly breathing, helps calm the nervous system and reduce stress. Lie on your back, place one hand on your chest and the other on your stomach. Inhale through your nose, feeling your stomach rise as your chest stays still. Exhale slowly, allowing your stomach to fall. This technique enhances oxygen exchange and calms the mind."
+        text:"Diaphragmatic breathing, or belly breathing, helps calm the nervous system and reduce stress. Lie on your back, place one hand on your chest and the other on your stomach. Inhale through your nose, feeling your stomach rise as your chest stays still. Exhale slowly, allowing your stomach to fall. This technique enhances oxygen exchange and calms the mind.",
+        isCompleted: false
     },
     {
         id: 2,
         name: "Alternate Nostril Breathing (Nadi Shodhana)",
         duration: 15,
         image: require("../assets/images/cards/breath2.png"),
-        text:"Nadi Shodhana is a practice for cleansing the breathing channels. Sit comfortably, close your right nostril with your right thumb, and inhale through the left nostril. Then close the left nostril and exhale through the right. Repeat for 5-10 minutes. This technique improves breathing, balances energy, and promotes mental clarity."
+        text:"Nadi Shodhana is a practice for cleansing the breathing channels. Sit comfortably, close your right nostril with your right thumb, and inhale through the left nostril. Then close the left nostril and exhale through the right. Repeat for 5-10 minutes. This technique improves breathing, balances energy, and promotes mental clarity.",
+        isCompleted: false
     },
     {
         id: 3,
         name: "4-7-8 Breathing",
         duration: 20,
         image: require("../assets/images/cards/breath3.png"),
-        text:"The 4-7-8 method is a technique to relax and manage anxiety. Inhale through your nose for a count of 4, hold your breath for a count of 7, and exhale slowly through your mouth for a count of 8. Repeat the cycle 4-5 times. This practice is especially useful before sleep, aiding relaxation and improving sleep quality."
+        text:"The 4-7-8 method is a technique to relax and manage anxiety. Inhale through your nose for a count of 4, hold your breath for a count of 7, and exhale slowly through your mouth for a count of 8. Repeat the cycle 4-5 times. This practice is especially useful before sleep, aiding relaxation and improving sleep quality.",
+        isCompleted: false
     }
 ]
