83d62fa6a4d949d12ae1bf1f4c0a8fa000e6fa48
Vachek-hooves <vachek0602@gmail.com>
2025-02-17 09:39:24 +0200

    Simplify WelcomeScreen navigation logic
    - Remove hardcoded date comparison
    - Streamline navigation to TabMenu
    - Clean up commented-out code and unused variables

--- screen/stack/WelcomeScreen.js
+++ screen/stack/WelcomeScreen.js
@@ -2,51 +2,12 @@ import {StyleSheet, Text, View, Animated} from 'react-native';
 import React, {useEffect, useRef, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import LinearGradient from 'react-native-linear-gradient';
-const todayDate = new Date();
-const hardCodeDate = new Date('2025-02-18T10:00:00');
 
 const WelcomeScreen = () => {
   const navigation = useNavigation();
   const loadingProgress = useRef(new Animated.Value(0)).current;
   const [percentage, setPercentage] = useState(0);
-  // const [route, setRoute] = useState(false);
-
-
-  // const INITIAL_URL = `https://brilliant-grand-happiness.space/`;
-  // const URL_IDENTIFAIRE = `9QNrrgg5`;
-  // const idfa = 'd1e5bd8c-a54d-4143-ad5e-7dd21cf238ff'
-  // useEffect(() => {
-  //   const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
-  //   //console.log(checkUrl);
-
-  //   const targetData = new Date('2025-01-14T10:00:00'); //дата з якої поч працювати webView
-  //   const currentData = new Date(); //текущая дата
-
-  //   if (!route) {
-  //     if (currentData <= targetData) {
-  //       setRoute(false);
-  //     } else {
-  //       fetch(checkUrl)
-  //         .then(r => {
-  //           if (r.status === 200) {
-  //             console.log('status по клоаке==>', r.status);
-
-  //             navigation.navigate('TestScreen');
-  //             // setRoute(true);
-  //           } else {
-  //             console.log(r.status);
-  //             navigation.replace('TabMenu');
-  //             // setRoute(false);
-  //           }
-  //         })
-  //         .catch(e => {
-  //           console.log('errar', e);
-  //           // setRoute(false);
-  //         });
-  //     }
-  //   }
-  // }, []);
-
+  
   useEffect(() => {
 
     // Add listener to update percentage display
@@ -59,11 +20,9 @@ const WelcomeScreen = () => {
       duration: 2000,
       useNativeDriver: false,
     }).start(() => {
-      if (todayDate <= hardCodeDate) {
+
         navigation.replace('TabMenu');
-      } else {
-        navigation.replace('TestScreen');
-      }
+     
     });
 
     // Cleanup listener
